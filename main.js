316624de2c840718bfff75b7e19142c3bd096e44
Leon9708 <Leon.ring@gmx.de>
2022-08-28 01:13:11 +0200

    second commit

--- main.js
+++ main.js
@@ -1,32 +1,55 @@
+let url = `https://pokeapi.co/api/v2/pokemon/`;
+let allPokemon = [];
 let currentPokemon;
 
 async function GetPokemons() {
-    let url = `https://pokeapi.co/api/v2/pokemon/pikachu`;
-    let response = await fetch(url);
-    currentPokemon = await response.json();
+    for (let i = 0; i < 15; i++) {
+        const pokemonUrl = url + (i + 1);
 
-    renderPokemon();
+        let response = await fetch(pokemonUrl);
+        currentPokemon = await response.json();
+        allPokemon.push(currentPokemon);
+        renderPokemon(i);
+        renderTypes(i);
+    }
+
+
 
     console.log('API answers', currentPokemon);
 }
 
-function renderPokemon() {
+function renderPokemon(i) {
     let boxPokemon = document.querySelector('.place_content');
     let pokemonImg = currentPokemon['sprites']['other']['dream_world']['front_default'];
 
     boxPokemon.innerHTML += /*html*/ `    
-    <div class="box_content">
- 
-        <div class="place_info">
-            <h1 class="pokemon_head">${currentPokemon['name']}</h1>
-            <h2 class="pokemon_num">#${currentPokemon['id']}</h2>
-            <div class="place_characteristics">
-                <img class="icon_characteristics" src="./img/leaf.ico">
-                <div  class="characteristics">
-                    Poison
+        <div class="box_content">
+            <div class="place_info">
+                <h1 class="pokemon_head">${currentPokemon['name']}</h1>
+                <h2 class="pokemon_num">#${currentPokemon['id']}</h2>
+                <div id="place_characteristics${i}" class="place_characteristics">
+
                 </div>
             </div>
-        </div>
-        <img class="pokemon_img" src="${pokemonImg}" alt="pokemon">
-    </div>`
+            <img class="pokemon_img" src="${pokemonImg}" alt="pokemon">
+        </div>`
+
+}
+
+
+function renderTypes(i) {
+    for (let j = 0; j < currentPokemon['types'].length; j++) {
+        const type = currentPokemon['types'][j];
+
+        document.getElementById('place_characteristics' + i).innerHTML += `
+            <div class="box_characteristics">
+                <div class="box_icon">
+                    <img class="icon_characteristics" src="icons/${type['type']['name']}.png">
+                </div>
+                <p  class="characteristics">
+                    ${type['type']['name']}
+                </p>
+             </div>`
+
+    }
 }
